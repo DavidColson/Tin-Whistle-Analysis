ed4750f4fd8081797941e82311323ce776fe32c1
David Colson <david@peripherallabs.com>
2017-03-18 13:06:44 +0000

    Plot Comparisons ideal scales
    Plots comparisons is now able to calculate ideal scales based on the
    first note of the simulated data.

--- PlotComparisons.m
+++ PlotComparisons.m
@@ -3,15 +3,15 @@
 % measurement
 
 function PlotComparisons(recordedCellData, shiftAmount)
-    simulatedData = [473.67;
-                    539.07;
-                    613.27;
-                    665.36;
-                    757.27;
-                    864.46;
-                    991.48;
-                    1000.2];
-                
+    simulatedData = [472.66;
+                    510.98;
+                    569.26;
+                    613.45;
+                    687.22;
+                    770.84;
+                    862.03;
+                    963];
+
     knownData = [466.164; 
                 523.251; 
                 587.33; 
@@ -20,7 +20,18 @@ function PlotComparisons(recordedCellData, shiftAmount)
                 783.991; 
                 880; 
                 932.328];
-
+     
+    % Valid for C Whistle Scale
+    sideal = zeros(8, 1)
+    sideal(1) = simulatedData(1);
+    sideal(2) = sideal(1)*(1.06)^2;
+    sideal(3) = sideal(2)*(1.06)^2;
+    sideal(4) = sideal(3)*(1.06)^1;
+    sideal(5) = sideal(4)*(1.06)^2;
+    sideal(6) = sideal(5)*(1.06)^2;
+    sideal(7) = sideal(6)*(1.06)^2;
+    sideal(8) = sideal(7)*(1.06)^1;
+    
     labels = transpose(recordedCellData(1, 1:8));
     x = linspace(1, 8, 8);
     labels = circshift(labels, shiftAmount);
@@ -30,7 +41,10 @@ function PlotComparisons(recordedCellData, shiftAmount)
     h(1) = plot(x, simulatedData, '-ob', 'LineWidth', 2, 'DisplayName', 'Simulated');
     
     hold on;
-    h(2) = plot(x, knownData, '-ok', 'LineWidth', 2, 'DisplayName', 'Tempered Scale');
+    %h(2) = plot(x, knownData, '-ok', 'LineWidth', 2, 'DisplayName', 'Tempered Scale');
+    
+    hold on;
+    h(2) = plot(x, sideal, '-oc', 'LineWidth', 2, 'DisplayName', 'Ideal Simulated Scale');
     
     % Plot average of recorded values with error bars
     avfrequencies = transpose(cell2mat(recordedCellData(8, 1:8)));
@@ -42,7 +56,7 @@ function PlotComparisons(recordedCellData, shiftAmount)
     set(gca,'XTickLabel',labels)
     xlim([1, 8]);
     ylabel('Note Frequency (Hz)');
-    title('Recorded Scale for Bb Whistle with errors');
+    title('Comparing with Ideal Semitone Note Ratios (Bb)');
     legend(h);
     hold off;
 end
